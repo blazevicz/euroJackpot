fd7766bed7b7016ab1a4cf16d2a07dc46efa9159
Damian <dddnsss@gmail.com>
2023-10-24 11:04:41 +0200

    Initial commit

---
+++ src/main/java/pl/dblazewicz/eurojackpot/EuroJackpotApplication.java
@@ -0,0 +1,13 @@
+package pl.dblazewicz.eurojackpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroJackpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EuroJackpotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dblazewicz/eurojackpot/EuroJackpotApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dblazewicz.eurojackpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroJackpotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
